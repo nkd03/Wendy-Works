9a1364afa6a4bc53242043aadaa8c6a37274b528
Noelle Davis class of 2025 <nd105@cs.wellesley.edu>
2023-11-17 13:27:43 -0500

    updated routing to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import (Flask, render_template, make_response, url_for, request,
                    redirect, flash, session, send_from_directory, jsonify)
 from werkzeug.utils import secure_filename
+import bcrypt
 app = Flask(__name__)
 
 # one or the other of these. Defaults to MySQL (PyMySQL)
@@ -28,42 +29,72 @@ def index():
 # You will probably not need the routes below, but they are here
 # just in case. Please delete them if you are not using them
 
-@app.route('/greet/', methods=["GET", "POST"])
-def greet():
-    if request.method == 'GET':
-        return render_template('greet.html', title='Customized Greeting')
-    else:
-        try:
-            username = request.form['username'] # throws error if there's trouble
-            flash('form submission successful')
-            return render_template('greet.html',
-                                   title='Welcome '+username,
-                                   name=username)
+@app.route('/join/', methods=["GET", "POST"])
+def join():
+     if request.method == 'GET':
+         return render_template('create.html', title='Create an Account')
+     
+    #  else:
+    #      try:
+    #         #getting account information first 
+    #          username = request.form.get("username") 
+    #          pass1=request.form.get("pswrd") 
+    #          pass2=request.form.get("pswrd-repeat")
+    #         #getting contact information
+    #          email=request.form.get("email")
 
-        except Exception as err:
-            flash('form submission error'+str(err))
-            return redirect( url_for('index') )
 
-@app.route('/formecho/', methods=['GET','POST'])
-def formecho():
-    if request.method == 'GET':
-        return render_template('form_data.html',
-                               method=request.method,
-                               form_data=request.args)
-    elif request.method == 'POST':
-        return render_template('form_data.html',
-                               method=request.method,
-                               form_data=request.form)
-    else:
-        # maybe PUT?
-        return render_template('form_data.html',
-                               method=request.method,
-                               form_data={})
+    #          if pass1 != pass2:
+    #             flash('passwords do not match')
+    #             return redirect( url_for('index'))
+#             flash('form submission successful')
+#             return render_template('greet.html',
+#                                    title='Welcome '+username,
+#                                    name=username)
 
-@app.route('/testform/')
-def testform():
-    # these forms go to the formecho route
-    return render_template('testform.html')
+        #  except Exception as err:
+        #      flash('form submission error'+str(err))
+        #      return redirect( url_for('index') )
+
+@app.route('/login/', methods = ["GET", "POST"])
+def login(): 
+    if request.method == 'GET': 
+        return render_template('login.html', title = 'Login to Wendy Works!')
+    else: 
+        user = ...
+        return redirect(url_for('profile', uid = user))
+    
+@app.route('/profile/<int:uid>')
+def profile(uid):
+    ...
+
+
+@app.route('/logout/')
+def after_logout():
+    flash("you've successfully logged out!")
+    return redirect( url_for('index') )
+
+# @app.route('/formecho/', methods=['GET','POST'])
+# def formecho():
+#     if request.method == 'GET':
+#         return render_template('form_data.html',
+#                                method=request.method,
+#                                form_data=request.args)
+#     elif request.method == 'POST':
+#         return render_template('form_data.html',
+#                                method=request.method,
+#                                form_data=request.form)
+#     else:
+#         # maybe PUT?
+#         return render_template('form_data.html',
+#                                method=request.method,
+#                                form_data={})
+
+# @app.route('/testform/')
+# def testform():
+#     # these forms go to the formecho route
+#     return render_template('testform.html')
+
 
 
 if __name__ == '__main__':
@@ -75,7 +106,7 @@ if __name__ == '__main__':
     else:
         port = os.getuid()
     # set this local variable to 'wmdb' or your personal or team db
-    db_to_use = 'put_database_name_here_db' 
+    db_to_use = 'wworks_db' 
     print('will connect to {}'.format(db_to_use))
     dbi.conf(db_to_use)
     app.debug = True
