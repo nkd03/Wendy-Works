f2feb0bad92a4c671b32be2b7ce7a6baa1b30ae6
Liz Maylin <ld102@cs.wellesley.edu>
2023-11-17 19:55:20 -0500

    changed new_account to pyqueries

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import (Flask, render_template, make_response, url_for, request,
                    redirect, flash, session, send_from_directory, jsonify)
 from werkzeug.utils import secure_filename
 import bcrypt
-import new_account as pyqueries
+import pyqueries
 app = Flask(__name__)
 
 # one or the other of these. Defaults to MySQL (PyMySQL)
 # change comment characters to switch to SQLite
 
 import cs304dbi as dbi
-import new_account
+
 # import cs304dbi_sqlite3 as dbi
 
 import random
@@ -64,17 +64,17 @@ def join():
             stored = hashed.decode('utf-8')
 
         #inserting into database
-            new_account.insert_new_user(conn,username,email,f_name,l_name,hashed)
+            pyqueries.insert_new_user(conn,username,email,f_name,l_name,hashed)
         #getting last uid
-            row = new_account.get_uid(conn)
+            row = pyqueries.get_uid(conn)
             uid = row.get("last_insert_id()")
             
             
         #inserting skills 
-            new_account.insert_skills(conn,uid,skills)
+            pyqueries.insert_skills(conn,uid,skills)
 
             if len(other_skills) > 0:
-                new_account.insert_other_skills(conn, uid, other_skills)
+                pyqueries.insert_other_skills(conn, uid, other_skills)
 
 
             flash('Account created! Now log in')
